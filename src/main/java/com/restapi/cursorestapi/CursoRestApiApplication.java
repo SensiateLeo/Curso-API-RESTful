a54dc5c6e35a9cd2d6800b0274147561aadbf1f7
Leonardo Sensiate <leonardo.sensiate@vistajet.com>
2020-08-21 13:13:26 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/restapi/cursorestapi/CursoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.cursorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoRestApiApplication.class, args);
+    }
+
+}
